b1d64f931184fe1e25f690b6b80ef2d5797fe637
lexuanhai <lexua@DESKTOP-QH0P113>
2019-10-28 14:16:39 +0700

    HaiLX commit full FE

---
+++ QLSX.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace QLSX.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ QLSX.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QLSX.Web.WebApiApplication" Language="C#" %>
