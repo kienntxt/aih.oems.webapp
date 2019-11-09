821ffd2b4e54a052f2d1193c2393919859e5b0ee
hungdoquang <hungdq.it@gmail.com>
2019-11-09 19:16:17 +0700

    HungDQ

--- QLSX.Web/Constant.cs
+++ QLSX.Web/Constant.cs
@@ -9,30 +9,30 @@ namespace OEMS.Web
     {
         public class CommodityTypes
         {
-            public const string Material = "Material";
-            public const string Product = "Product";
-            public const string SemiProduct = "Semi";
+            public const string Material = "material";
+            public const string Product = "product";
+            public const string SemiProduct = "semi";
             //public string Code { get; set; }
             //public string Name { get; set; }
             //public static List<string> lstCode = new List<string> { "Material", "Product", "Semi-Product" };
             public static readonly Dictionary<string, string> dctCommdityTypes = new Dictionary<string, string>()
             {
-                {"Material", "Vật tư"},
-                {"Product", "Thành phẩm"},
-                {"Semi", "Bán thành phẩm"},
+                {"material", "Vật tư"},
+                {"product", "Thành phẩm"},
+                {"semi", "Bán thành phẩm"},
             };
         }
 
         public class PartnerTypes
         {
-            public const string Partner = "Partner";
-            public const string Vendor = "Vendor";
-            public const string Customer = "Customer";
+            public const string Partner = "partner";
+            public const string Vendor = "vendor";
+            public const string Customer = "customer";
             public static readonly Dictionary<string, string> dctPartnerTypes = new Dictionary<string, string>()
             {
-                {"Partner", "Nhà cung cấp - Khách hàng"},
-                {"Vendor", "Nhà cung cấp"},
-                {"Customer", "Khách hàng"},
+                {"vartner", "Nhà cung cấp - Khách hàng"},
+                {"vendor", "Nhà cung cấp"},
+                {"customer", "Khách hàng"},
             };
         }
     }
